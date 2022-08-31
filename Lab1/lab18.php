3da0fd7ca5303050e75d5c42c0e956f8c1a3c02b
jfdla09 <112501313+jfdla09@users.noreply.github.com>
2022-08-30 19:47:27 -0500

    Add files via upload

---
+++ Lab1/lab18.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>laboratorio 1.8</title>
+</head>
+<body>
+    <?php
+        $a = 8;
+        $b = 3;
+        echo $a + $b, "<br>";
+        echo $a - $b, "<br>";
+        echo $a * $b, "<br>";
+        echo $a / $b, "<br>";
+        $a++; echo $a, "<br>";
+        $b--; echo $b, "<br>";
+    ?>
+</body>
+</html>
